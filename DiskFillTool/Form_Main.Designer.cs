b75d0b0e41eb3676594cdb8e57bcd1a6e9bf2c3a
sam <hi@hesihan.com>
2017-03-14 00:51:58 +0800

    Disk Fill Tool

---
+++ DiskFillTool/Form_Main.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace DiskFillTool
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar_Fill = new System.Windows.Forms.ProgressBar();
+            this.comboBox_PartitionName = new System.Windows.Forms.ComboBox();
+            this.button_Fill = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_UsedSpaceSize = new System.Windows.Forms.Label();
+            this.label_UsedSpacePercentage = new System.Windows.Forms.Label();
+            this.progressBar_UsedSpace = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label_FreeSpaceSize = new System.Windows.Forms.Label();
+            this.label_FreeSpacePercentage = new System.Windows.Forms.Label();
+            this.progressBar_FreeSpace = new System.Windows.Forms.ProgressBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label_TotalSize = new System.Windows.Forms.Label();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar_Fill
+            // 
+            this.progressBar_Fill.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar_Fill.Location = new System.Drawing.Point(0, 288);
+            this.progressBar_Fill.Margin = new System.Windows.Forms.Padding(0);
+            this.progressBar_Fill.Name = "progressBar_Fill";
+            this.progressBar_Fill.Size = new System.Drawing.Size(384, 23);
+            this.progressBar_Fill.TabIndex = 0;
+            // 
+            // comboBox_PartitionName
+            // 
+            this.comboBox_PartitionName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_PartitionName.FormattingEnabled = true;
+            this.comboBox_PartitionName.Location = new System.Drawing.Point(238, 65);
+            this.comboBox_PartitionName.Name = "comboBox_PartitionName";
+            this.comboBox_PartitionName.Size = new System.Drawing.Size(121, 20);
+            this.comboBox_PartitionName.TabIndex = 1;
+            // 
+            // button_Fill
+            // 
+            this.button_Fill.Location = new System.Drawing.Point(238, 103);
+            this.button_Fill.Name = "button_Fill";
+            this.button_Fill.Size = new System.Drawing.Size(121, 23);
+            this.button_Fill.TabIndex = 2;
+            this.button_Fill.Text = "Fill";
+            this.button_Fill.UseVisualStyleBackColor = true;
+            this.button_Fill.Click += new System.EventHandler(this.button_Fill_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label_UsedSpaceSize);
+            this.groupBox1.Controls.Add(this.label_UsedSpacePercentage);
+            this.groupBox1.Controls.Add(this.progressBar_UsedSpace);
+            this.groupBox1.Location = new System.Drawing.Point(12, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Used Space(MB)";
+            // 
+            // label_UsedSpaceSize
+            // 
+            this.label_UsedSpaceSize.AutoSize = true;
+            this.label_UsedSpaceSize.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_UsedSpaceSize.Location = new System.Drawing.Point(6, 32);
+            this.label_UsedSpaceSize.Name = "label_UsedSpaceSize";
+            this.label_UsedSpaceSize.Size = new System.Drawing.Size(16, 16);
+            this.label_UsedSpaceSize.TabIndex = 4;
+            this.label_UsedSpaceSize.Text = "0";
+            // 
+            // label_UsedSpacePercentage
+            // 
+            this.label_UsedSpacePercentage.AutoSize = true;
+            this.label_UsedSpacePercentage.Location = new System.Drawing.Point(165, 76);
+            this.label_UsedSpacePercentage.Name = "label_UsedSpacePercentage";
+            this.label_UsedSpacePercentage.Size = new System.Drawing.Size(17, 12);
+            this.label_UsedSpacePercentage.TabIndex = 1;
+            this.label_UsedSpacePercentage.Text = "0%";
+            // 
+            // progressBar_UsedSpace
+            // 
+            this.progressBar_UsedSpace.Location = new System.Drawing.Point(6, 71);
+            this.progressBar_UsedSpace.Name = "progressBar_UsedSpace";
+            this.progressBar_UsedSpace.Size = new System.Drawing.Size(153, 23);
+            this.progressBar_UsedSpace.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_FreeSpaceSize);
+            this.groupBox2.Controls.Add(this.label_FreeSpacePercentage);
+            this.groupBox2.Controls.Add(this.progressBar_FreeSpace);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Free Space(MB)";
+            // 
+            // label_FreeSpaceSize
+            // 
+            this.label_FreeSpaceSize.AutoSize = true;
+            this.label_FreeSpaceSize.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_FreeSpaceSize.Location = new System.Drawing.Point(6, 34);
+            this.label_FreeSpaceSize.Name = "label_FreeSpaceSize";
+            this.label_FreeSpaceSize.Size = new System.Drawing.Size(16, 16);
+            this.label_FreeSpaceSize.TabIndex = 1;
+            this.label_FreeSpaceSize.Text = "0";
+            // 
+            // label_FreeSpacePercentage
+            // 
+            this.label_FreeSpacePercentage.AutoSize = true;
+            this.label_FreeSpacePercentage.Location = new System.Drawing.Point(165, 76);
+            this.label_FreeSpacePercentage.Name = "label_FreeSpacePercentage";
+            this.label_FreeSpacePercentage.Size = new System.Drawing.Size(17, 12);
+            this.label_FreeSpacePercentage.TabIndex = 3;
+            this.label_FreeSpacePercentage.Text = "0%";
+            // 
+            // progressBar_FreeSpace
+            // 
+            this.progressBar_FreeSpace.Location = new System.Drawing.Point(6, 71);
+            this.progressBar_FreeSpace.Name = "progressBar_FreeSpace";
+            this.progressBar_FreeSpace.Size = new System.Drawing.Size(153, 23);
+            this.progressBar_FreeSpace.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label_TotalSize);
+            this.groupBox3.Location = new System.Drawing.Point(12, 225);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 60);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Total Size(MB)";
+            // 
+            // label_TotalSize
+            // 
+            this.label_TotalSize.AutoSize = true;
+            this.label_TotalSize.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_TotalSize.Location = new System.Drawing.Point(6, 26);
+            this.label_TotalSize.Name = "label_TotalSize";
+            this.label_TotalSize.Size = new System.Drawing.Size(16, 16);
+            this.label_TotalSize.TabIndex = 0;
+            this.label_TotalSize.Text = "0";
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Enabled = false;
+            this.button_Stop.Location = new System.Drawing.Point(238, 189);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(121, 23);
+            this.button_Stop.TabIndex = 6;
+            this.button_Stop.Text = "Stop";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 311);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_Fill);
+            this.Controls.Add(this.comboBox_PartitionName);
+            this.Controls.Add(this.progressBar_Fill);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form_Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Disk Fill Tool";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_Main_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar_Fill;
+        private System.Windows.Forms.ComboBox comboBox_PartitionName;
+        private System.Windows.Forms.Button button_Fill;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label_UsedSpacePercentage;
+        private System.Windows.Forms.ProgressBar progressBar_UsedSpace;
+        private System.Windows.Forms.Label label_FreeSpacePercentage;
+        private System.Windows.Forms.ProgressBar progressBar_FreeSpace;
+        private System.Windows.Forms.Label label_TotalSize;
+        private System.Windows.Forms.Label label_UsedSpaceSize;
+        private System.Windows.Forms.Label label_FreeSpaceSize;
+        private System.Windows.Forms.Button button_Stop;
+    }
+}
+
---
+++ DiskFillTool/Form_Main.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DiskFillTool
+{
+    public partial class Form_Main : Form
+    {
+        private DriveInfo driveInfo = null;
+        private long freeSize;
+        private Thread _zero = null;
+        private Thread _one = null;
+        private Thread _monitor = null;
+
+        public Form_Main()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            this.GetThePartitionNameAndFillTheComboBox();
+        }
+
+        private void GetTheDriveInfoAndSetTheControlValue()
+        {
+            while (true)
+            {
+                long totalFreeSpace = this.driveInfo.TotalFreeSpace;
+                long totalSize = this.driveInfo.TotalSize;
+
+                if (totalFreeSpace <= 734003200)
+                {
+                    MessageBox.Show("Complete");
+                    this.CloseAllThreads();
+                    break;
+                }
+
+                this.label_FreeSpaceSize.Text = (totalFreeSpace / 1024.0 / 1024.0).ToString();
+                this.progressBar_FreeSpace.Value = (int)((double)totalFreeSpace / totalSize * 100.0);
+                this.label_FreeSpacePercentage.Text = this.progressBar_FreeSpace.Value.ToString() + "%";
+
+                this.label_UsedSpaceSize.Text = ((totalSize - totalFreeSpace) / 1024.0 / 1024.0).ToString();
+                this.progressBar_UsedSpace.Value = (int)((double)(totalSize - totalFreeSpace) / totalSize * 100.0);
+                this.label_UsedSpacePercentage.Text = this.progressBar_UsedSpace.Value.ToString() + "%";
+
+                this.label_TotalSize.Text = (totalSize / 1024.0 / 1024.0).ToString();
+
+                this.progressBar_Fill.Value = (int)((double)(this.freeSize - totalFreeSpace) / this.freeSize * 100.0);
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        private void GetThePartitionNameAndFillTheComboBox()
+        {
+            string[] driveName = Environment.GetLogicalDrives();
+            this.comboBox_PartitionName.DataSource = driveName;
+        }
+
+        private void SetTheControlEnabledState(string str)
+        {
+            switch (str)
+            {
+                case "Start":
+                    this.comboBox_PartitionName.Enabled = false;
+                    this.button_Fill.Enabled = false;
+                    this.button_Stop.Enabled = true;
+                    break;
+                case "Stop":
+                    this.comboBox_PartitionName.Enabled = true;
+                    this.button_Fill.Enabled = true;
+                    this.button_Stop.Enabled = false;
+                    this.button_Stop.Text = "Stop";
+                    break;
+                case "Stopping":
+                    this.button_Stop.Text = "Stopping";
+                    this.button_Stop.Enabled = false;
+                    break;
+            }
+        }
+
+        private void CreateThreadsAndBeginToFill()
+        {
+            this.driveInfo = new DriveInfo(this.comboBox_PartitionName.Text);
+            this.freeSize = this.driveInfo.TotalFreeSpace;
+
+            this._monitor = new Thread(new ThreadStart(GetTheDriveInfoAndSetTheControlValue));
+            _monitor.Start();
+            this._zero = new Thread(new ThreadStart(FillZero));
+            _zero.Start();
+            this._one = new Thread(new ThreadStart(FillOne));
+            _one.Start();
+        }
+
+        private void CloseAllThreads()
+        {
+            if (this._zero == null || this._one == null || this._monitor == null)
+            {
+                return;
+            }
+            this._zero.Abort();
+            this._one.Abort();
+            this._monitor.Abort();
+            while (true)
+            {
+                if (this._zero.IsAlive == false && this._one.IsAlive == false && this._monitor.IsAlive == false)
+                {
+                    break;
+                }
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void FillZero()
+        {
+            UInt64 count = 0;
+            while(true)
+            {
+                FileStream fs = null;
+                try
+                {
+                    fs = new FileStream(this.driveInfo.Name + "0" + count++, FileMode.OpenOrCreate);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    this.CloseAllThreads();
+                }
+                StreamWriter sw = new StreamWriter(fs);
+                long end = DateTime.Now.Millisecond * 1000000;
+                for (long i = 0; i < end; i++)
+                {
+                    sw.Write("0");
+                    if (i % 100000000 == 0)
+                    {
+                        Thread.Sleep(500);
+                    }
+                }
+                sw.Close();
+            }
+        }
+
+        private void FillOne()
+        {
+            UInt64 count = 0;
+            while (true)
+            {
+                try
+                {
+                    FileStream fs = new FileStream(this.driveInfo.Name + "1" + count++, FileMode.OpenOrCreate);
+                    StreamWriter sw = new StreamWriter(fs);
+                    long end = DateTime.Now.Millisecond * 1000000;
+                    for (long i = 0; i < end; i++)
+                    {
+                        sw.Write("1");
+                        if (i % 100000000 == 0)
+                        {
+                            Thread.Sleep(500);
+                        }
+                    }
+                    sw.Close();
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void button_Fill_Click(object sender, EventArgs e)
+        {
+            this.SetTheControlEnabledState("Start");
+            this.CreateThreadsAndBeginToFill();
+        }
+
+        private void button_Stop_Click(object sender, EventArgs e)
+        {
+            this.SetTheControlEnabledState("Stopping");
+            this.CloseAllThreads();
+            this.SetTheControlEnabledState("Stop");
+        }
+
+        private void Form_Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.CloseAllThreads();
+        }
+    }
+}
